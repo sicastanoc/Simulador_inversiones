87faa22acc1cab48edf45215adcccbf47c43615c
Simon Castaño Correa <102295852+sicastanoc@users.noreply.github.com>
2024-06-23 18:17:46 -0500

    se arregla el js de modificar usuario

--- js/verificar_usuario.js
+++ js/verificar_usuario.js
@@ -1,28 +1,22 @@
+// Función para enviar datos al servidor
 async function enviarDatos() {
-    const nombreUsuario = document.getElementById('nombreUsuario').value;
-    const url = `http://127.0.0.1:8000/users/${nombreUsuario}`;
-    
-    try {
+  event.preventDefault(); // Prevenir el envío del formulario por defecto
+  
+  const nombreUsuario = document.getElementById('nombreUsuario').value;
+  const url = `http://127.0.0.1:8000/users/${nombreUsuario}`; // URL de la API
+  
+  try {
       const response = await fetch(url);
+      
       if (!response.ok) {
-        // Si la respuesta no es exitosa (código de estado no está en el rango 200-299)
-        if (response.status === 404) {
           throw new Error('Usuario no encontrado');
-        } else {
-          throw new Error('Error en la solicitud: ' + response.status);
-        }
       }
       
-      // Redirigir a la página trading.html solo si la respuesta es exitosa
+      // Si la respuesta es exitosa (código 200), redirigir a trading.html
       window.location.href = 'trading.html';
       
-    } catch (error) {
-      // Manejar errores
-      if (error.message.includes('404')) {
-        document.getElementById('result').textContent = 'Usuario no encontrado';
-        // No redirigir automáticamente en caso de error 404
-      } else {
-        document.getElementById('result').textContent = error.message;
-      }
-    }
+  } catch (error) {
+      // Capturar y manejar errores de la API
+      document.getElementById('result').textContent = error.message;
   }
+}
