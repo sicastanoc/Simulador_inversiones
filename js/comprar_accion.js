8c3d51c8bce112121c32d3bc210705a777ed60b7
Simon Castaño Correa <102295852+sicastanoc@users.noreply.github.com>
2024-06-25 19:49:17 -0500

    se pone mas bonito el css

---
+++ js/comprar_accion.js
@@ -0,0 +1,18 @@
+// Función para obtener y mostrar los precios de las acciones
+async function mostrarPrecios(accion_id,id_precio_accion) {
+    const response = await fetch(`http://127.0.0.1:8000/acciones/${accion_id}`); // Reemplaza {accion_id} con el ID de la acción específica
+    const data = await response.json();
+
+    const preciosContainer = document.getElementById(`${id_precio_accion}`);
+    preciosContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevos datos
+
+    data.forEach(precio => {
+        const precioHTML = `
+            <div class="precio-item">
+                <h5>${precio.nombre_accion} (${precio.nombre_abreviado}) </h5>
+                <p>Precio: ${precio.precio}</p>
+            </div>
+        `;
+        preciosContainer.innerHTML += precioHTML;
+    });
+}
